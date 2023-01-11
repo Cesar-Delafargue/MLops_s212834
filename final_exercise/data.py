2fab082c70f8c56e070124b54b1b64b29f6a0514
Cesar-Delafargue <cesar.delafargue@gmail.com>
2023-01-12 00:17:34 +0100

    Added some testing

--- final_exercise/data.py
+++ final_exercise/data.py
@@ -3,6 +3,7 @@ import torch
 
 def mnist():
     # exchange with the corrupted mnist dataset
-    train = torch.randn(50000, 784)
-    test = torch.randn(10000, 784) 
+    train = torch.randn(40000, 784)
+    test = torch.randn(5000, 784) 
+    #change
     return train, test
